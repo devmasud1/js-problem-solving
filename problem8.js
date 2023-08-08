d4e705991e9970171404ccfab8aaed9b748baf95
devmasud1 <devmahmud.masu1@gmail.com>
2023-08-08 15:20:19 +0600

    problem-8

---
+++ problem8.js
@@ -0,0 +1,27 @@
+/*
+Problem 1:
+আপনার কাছে একটি জাভাস্ক্রিপ্ট ফাংশন রয়েছে যার নাম হলো isValidPhotoName(photoName, imageExtensions)।
+ এই ফাংশনটি দুটি প্যারামিটার গ্রহণ করে: photoName, যা একটি ছবির নাম প্রদর্শন করে, এবং imageExtensions Array, যা .jpg, .png, ইত্যাদি ছবির এক্সটেনশন গুলি ধারণ করে।
+আপনার কাজ হলো isValidPhotoName ফাংশনটি সম্পূর্ণ করা যাতে এই ফাংশনের মাধ্যমে পরীক্ষা করা যায় আপনি দেওয়া প্যারামিটার photoName সঠিক ছবির নাম কিনা তা। এই ফাংশনের কাজ হলো true রিটার্ন করা যদি photoName একটি বৈধ ছবির নামের শেষে বিদ্যমান ছবির এক্সটেনশন গুলির মধ্যে যেকোনটি শেষ হয়; অন্যথায়, ফাংশনটি false রিটার্ন করবে।
+
+*/
+
+// .jpg .jpeg .png .gif .ico .JPEG
+
+function isValidPhotoName(imgName, imgExtensions) {
+  if (typeof imgName !== "string" || !Array.isArray(imgExtensions)) {
+    return "provide valid inputs";
+  } else {
+    for (const item of imgExtensions) {
+      if (imgName.toLowerCase().endsWith(item.toLowerCase())) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+const photoName = "img.png";
+const imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".ico", ".JPEG"];
+
+console.log(isValidPhotoName(photoName, imageExtensions));
